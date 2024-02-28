0ad5bc778d6a72bac8a073019aa2d00c9f1f759a
Pasupuleti Pavan kumar <125397570+pavan0099@users.noreply.github.com>
2024-02-28 11:48:14 +0530

    Update Pneumonia_Classifier.py

--- Tabs/Pneumonia_Classifier.py
+++ Tabs/Pneumonia_Classifier.py
@@ -25,7 +25,7 @@ def app():
 
     uploaded_file = st.file_uploader("Choose a File", type=['jpg','png','jpeg'])
 
-    score = np.load('BreadcrumbsPneumonia_detection_and_prediction/Model1_acc.npy')
+    score = np.load('Pneumonia_detection_and_prediction/Model1_acc.npy')
     
     if uploaded_file!=None:
         st.image(uploaded_file)
